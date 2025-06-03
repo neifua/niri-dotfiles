f18545e306429be6d8a2488e92ad570d1e24cbfa
Neifua <ogkhang.05@gmail.com>
2025-06-04 00:22:19 +0700

    configs

--- config/swayidle/autolock.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Lock screen after 2 minutes of idle, and also before suspend
-swayidle -w \
-  timeout 300 '$HOME/.config/swaylock/lockscreen.sh' \
-  before-sleep '$HOME/.config/swaylock/lockscreen.sh'
