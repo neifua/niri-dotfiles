7961b0baa1c3622bbf9298c7ab6eadb38eca96b8
Neifua <ogkhang.05@gmail.com>
2025-06-30 12:28:35 +0700

    .

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -1,5 +1,7 @@
 return {
-  "nvim-lua/plenary.nvim",
+  { import = "nvchad.blink.lazyspec" },
+
+  { "nvim-lua/plenary.nvim", lazy = true },
 
   {
     "nvchad/base46",
@@ -21,14 +23,6 @@ return {
   { "nvzone/minty", cmd = { "Huefy", "Shades" } },
 
   {
-    "nvim-tree/nvim-web-devicons",
-    opts = function()
-      dofile(vim.g.base46_cache .. "devicons")
-      return { override = require "nvchad.icons.devicons" }
-    end,
-  },
-
-  {
     "lukas-reineke/indent-blankline.nvim",
     event = "User FilePost",
     opts = {
@@ -46,7 +40,6 @@ return {
     end,
   },
 
-  -- file managing , picker etc
   {
     "nvim-tree/nvim-tree.lua",
     cmd = { "NvimTreeToggle", "NvimTreeFocus" },
@@ -65,7 +58,6 @@ return {
     end,
   },
 
-  -- git stuff
   {
     "lewis6991/gitsigns.nvim",
     event = "User FilePost",
@@ -74,7 +66,6 @@ return {
     end,
   },
 
-  -- lsp stuff
   {
     "mason-org/mason.nvim",
     cmd = { "Mason", "MasonInstall", "MasonUpdate" },
@@ -85,19 +76,16 @@ return {
 
   {
     "neovim/nvim-lspconfig",
-    event = "User FilePost",
     config = function()
-      require("nvchad.configs.lspconfig").defaults()
+      require "configs.lspconfig"
     end,
   },
 
-  -- load luasnips + cmp related in insert mode only
   {
     "hrsh7th/nvim-cmp",
     event = "InsertEnter",
     dependencies = {
       {
-        -- snippet plugin
         "L3MON4D3/LuaSnip",
         dependencies = "rafamadriz/friendly-snippets",
         opts = { history = true, updateevents = "TextChanged,TextChangedI" },
@@ -107,7 +95,6 @@ return {
         end,
       },
 
-      -- autopairing of (){}[] etc
       {
         "windwp/nvim-autopairs",
         opts = {
@@ -117,13 +104,11 @@ return {
         config = function(_, opts)
           require("nvim-autopairs").setup(opts)
 
-          -- setup cmp for autopairs
           local cmp_autopairs = require "nvim-autopairs.completion.cmp"
           require("cmp").event:on("confirm_done", cmp_autopairs.on_confirm_done())
         end,
       },
 
-      -- cmp sources plugins
       {
         "saadparwaiz1/cmp_luasnip",
         "hrsh7th/cmp-nvim-lua",
@@ -233,6 +218,411 @@ return {
     opts = {
       defaults = {
         ["<leader>cF"] = { name = "+Format Injected Langs" },
+      },
+    },
+  },
+
+  {
+    "stevearc/conform.nvim",
+    event = { "BufWritePre" },
+    cmd = { "ConformInfo" },
+    keys = {
+      {
+        "<leader>cF",
+        function()
+          require("conform").format {
+            formatters = { "injected" },
+            timeout_ms = 3000,
+          }
+        end,
+        mode = { "n", "v" },
+        desc = "Format Injected Langs",
+      },
+    },
+    opts = {
+      format_on_save = {
+        timeout_ms = 3000,
+        lsp_fallback = true,
+      },
+
+      formatters_by_ft = {
+        lua = { "stylua" },
+        sh = { "beautysh" },
+        bash = { "beautysh" },
+        zsh = { "beautysh" },
+        python = { "black" },
+        javascript = { "prettier" },
+        typescript = { "prettier" },
+        javascriptreact = { "prettier" },
+        typescriptreact = { "prettier" },
+        json = { "prettier" },
+        jsonc = { "prettier" },
+        yaml = { "prettier" },
+        html = { "prettier" },
+        css = { "prettier" },
+        scss = { "prettier" },
+        markdown = { "prettier" },
+        markdown_inline = { "prettier" },
+        toml = { "pyproject-fmt" },
+        rust = { "ast-grep" },
+        go = { "ast-grep" },
+        c = { "clang-format" },
+        cpp = { "clang-format" },
+        cs = { "clang-format" },
+        cmake = { "cmakelang" },
+        java = { "clang_format" },
+      },
+
+      formatters = {
+        injected = { options = { ignore_errors = true } },
+      },
+    },
+  },
+
+  {
+    "goolord/alpha-nvim",
+    event = "VimEnter",
+    config = function()
+      -- Define custom highlight groups
+      vim.api.nvim_set_hl(0, "AlphaHeader", { fg = "#c4a7e7", bold = true })
+      vim.api.nvim_set_hl(0, "AlphaButtons", { fg = "#9ccfd8" })
+      vim.api.nvim_set_hl(0, "AlphaShortcut", { fg = "#eb6f92", italic = true })
+
+      local dashboard = require "alpha.themes.dashboard"
+
+      local logo = [[
+                 /'`\             /|\
+               ,' :  `\  ,      /' | `\
+              .'  :    `/| /| /'  /'   |
+           ._ |   `\   |'|/'//'  |     | ,
+           `\`-\    |  |/' |'|   `\    |/|
+            /`\_`-,_|_-'_,-'/|     `\,/'/'
+           |/~~~~~~\ /'~  /' `\___-~/_,'
+                    Y     `~`~\_Y ~~~
+                     `\/        |_
+        _______       -`,/    \/'
+     _/~~    \ ~\       |    /'
+   /' ~/~~~~~\|  `~\  _.' _/'\
+ /'_/~|    /'~`\   |\  |_ |
+|/~_/~~~\/'     `\ | `\| /'\
+|/'     `\   /~~~~`\____\'
+          `~'\___ _/__,-|
+                 ~~'   -|_
+                        |
+  ]]
+
+      dashboard.section.header.val = vim.split(logo, "\n")
+      dashboard.section.header.opts.hl = "AlphaHeader"
+
+      dashboard.section.buttons.val = {
+        dashboard.button("f", " " .. " Find File", "<cmd>Telescope find_files no_ignore=true hidden=true<cr>"),
+        dashboard.button("n", " " .. " New File", "<cmd>ene <BAR> startinsert<cr>"),
+        dashboard.button("r", " " .. " Recent Files", "<cmd>Telescope oldfiles<cr>"),
+        dashboard.button("g", " " .. " Find Text", "<cmd>Telescope live_grep<cr>"),
+        dashboard.button("q", " " .. " Quit", "<cmd>qa<cr>"),
+      }
+
+      for _, button in ipairs(dashboard.section.buttons.val) do
+        button.opts.hl = "AlphaButtons"
+        button.opts.hl_shortcut = "AlphaShortcut"
+      end
+
+      dashboard.section.footer.opts.hl = "AlphaFooter"
+      dashboard.section.buttons.opts.hl = "AlphaButtons"
+      dashboard.opts.layout[1].val = 6
+
+      require("alpha").setup(dashboard.opts)
+    end,
+  },
+
+  {
+    "mfussenegger/nvim-lint",
+    event = "BufReadPre",
+    config = function()
+      local lint = require "lint"
+
+      lint.linters_by_ft = {
+        fish = { "fish" },
+        lua = { "luacheck" },
+        python = { "flake8" },
+      }
+
+      local function debounce(ms, fn)
+        local timer = vim.uv.new_timer()
+        return function(...)
+          local argv = { ... }
+          timer:start(ms, 0, function()
+            timer:stop()
+            vim.schedule_wrap(fn)(unpack(argv))
+          end)
+        end
+      end
+
+      local function run_lint()
+        local ft = vim.bo.filetype
+        local names = lint._resolve_linter_by_ft(ft)
+        names = vim.list_extend({}, names)
+
+        if #names == 0 then
+          vim.list_extend(names, lint.linters_by_ft["_"] or {})
+        end
+
+        vim.list_extend(names, lint.linters_by_ft["*"] or {})
+
+        local ctx = { filename = vim.api.nvim_buf_get_name(0) }
+        ctx.dirname = vim.fn.fnamemodify(ctx.filename, ":h")
+        names = vim.tbl_filter(function(name)
+          local linter = lint.linters[name]
+          return linter and not (type(linter) == "table" and linter.condition and not linter.condition(ctx))
+        end, names)
+
+        if #names > 0 then
+          lint.try_lint(names)
+        end
+      end
+
+      vim.api.nvim_create_autocmd({ "BufWritePost", "BufReadPost", "InsertLeave" }, {
+        group = vim.api.nvim_create_augroup("nvim-lint", { clear = true }),
+        callback = debounce(100, run_lint),
+      })
+    end,
+  },
+
+  { "nvim-tree/nvim-web-devicons", enabled = false },
+
+  {
+    "echasnovski/mini.icons",
+    lazy = true,
+    init = function()
+      package.preload["nvim-web-devicons"] = function()
+        require("mini.icons").mock_nvim_web_devicons()
+        return package.loaded["nvim-web-devicons"]
+      end
+    end,
+    opts = {
+      file = {
+        [".keep"] = { glyph = "󰊢", hl = "MiniIconsGrey" },
+        ["devcontainer.json"] = { glyph = "", hl = "MiniIconsAzure" },
+      },
+      filetype = {
+        dotenv = { glyph = "", hl = "MiniIconsYellow" },
+      },
+    },
+  },
+
+  {
+    "folke/noice.nvim",
+    event = "VeryLazy",
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+      "rcarriga/nvim-notify",
+    },
+    config = function()
+      require("noice").setup {
+        cmdline = {
+          enabled = true,
+          view = "cmdline_popup",
+          format = {
+            cmdline = { pattern = "^:", icon = "", lang = "vim" },
+            search_down = { kind = "search", pattern = "^/", icon = "🔍 ", lang = "regex" },
+            search_up = { kind = "search", pattern = "^%?", icon = "🔍 ", lang = "regex" },
+          },
+        },
+        messages = {
+          enabled = true,
+        },
+        popupmenu = {
+          enabled = true,
+        },
+        presets = {
+          bottom_search = true,
+          command_palette = true,
+          long_message_to_split = true,
+        },
+      }
+    end,
+  },
+
+  {
+    "rcarriga/nvim-notify",
+    config = function()
+      vim.notify = require "notify"
+    end,
+  },
+
+  {
+    "folke/persistence.nvim",
+    event = "BufReadPre",
+    opts = {},
+  -- stylua: ignore
+  keys = {
+    { "<leader>qs", function() require("persistence").load() end, desc = "Restore Session" },
+    { "<leader>qS", function() require("persistence").select() end,desc = "Select Session" },
+    { "<leader>ql", function() require("persistence").load({ last = true }) end, desc = "Restore Last Session" },
+    { "<leader>qd", function() require("persistence").stop() end, desc = "Don't Save Current Session" },
+  },
+  },
+
+  {
+    "hrsh7th/nvim-cmp",
+    event = "InsertEnter",
+    dependencies = {
+      {
+        "L3MON4D3/LuaSnip",
+        build = not vim.loop.os_uname().sysname:match "Windows"
+            and "echo 'NOTE: jsregexp is optional'; make install_jsregexp"
+          or nil,
+        dependencies = {
+          {
+            "rafamadriz/friendly-snippets",
+            config = function()
+              require("luasnip.loaders.from_vscode").lazy_load()
+              require("luasnip.loaders.from_vscode").lazy_load {
+                paths = { vim.fn.stdpath "config" .. "/snippets" },
+              }
+            end,
+          },
+        },
+        config = function()
+          require("luasnip").config.set_config {
+            history = true,
+            delete_check_events = "TextChanged",
+          }
+
+          _G.LazyVim = _G.LazyVim or {}
+          LazyVim.cmp = LazyVim.cmp or {}
+          LazyVim.cmp.actions = LazyVim.cmp.actions or {}
+
+          LazyVim.cmp.actions.snippet_forward = function()
+            if require("luasnip").jumpable(1) then
+              vim.schedule(function()
+                require("luasnip").jump(1)
+              end)
+              return true
+            end
+          end
+
+          LazyVim.cmp.actions.snippet_stop = function()
+            if require("luasnip").expand_or_jumpable() then
+              require("luasnip").unlink_current()
+              return true
+            end
+          end
+        end,
+      },
+
+      { "saadparwaiz1/cmp_luasnip" },
+      { "hrsh7th/cmp-nvim-lsp" },
+      { "hrsh7th/cmp-buffer" },
+      { "hrsh7th/cmp-path" },
+      { "hrsh7th/cmp-cmdline" },
+    },
+    opts = function()
+      local cmp = require "cmp"
+      return {
+        snippet = {
+          expand = function(args)
+            require("luasnip").lsp_expand(args.body)
+          end,
+        },
+        mapping = cmp.mapping.preset.insert {
+          ["<Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_next_item()
+            elseif require("luasnip").expand_or_jumpable() then
+              require("luasnip").expand_or_jump()
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
+          ["<S-Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_prev_item()
+            elseif require("luasnip").jumpable(-1) then
+              require("luasnip").jump(-1)
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
+        },
+        sources = {
+          { name = "nvim_lsp" },
+          { name = "luasnip" },
+          { name = "buffer" },
+          { name = "path" },
+        },
+      }
+    end,
+  },
+
+  {
+    "gbprod/yanky.nvim",
+    event = "BufReadPost",
+    opts = {
+      highlight = { timer = 150 },
+    },
+    keys = {
+      {
+        "<leader>p",
+        function()
+          local ok, pick = pcall(require, "telescope._extensions.yank_history")
+          if ok then
+            require("telescope").extensions.yank_history.yank_history {}
+          else
+            vim.cmd "YankyRingHistory"
+          end
+        end,
+        mode = { "n", "x" },
+        desc = "Open Yank History",
+      },
+
+      -- Basic yank/paste
+      { "y", "<Plug>(YankyYank)", mode = { "n", "x" }, desc = "Yank Text" },
+      { "p", "<Plug>(YankyPutAfter)", mode = { "n", "x" }, desc = "Put Text After Cursor" },
+      { "P", "<Plug>(YankyPutBefore)", mode = { "n", "x" }, desc = "Put Text Before Cursor" },
+      { "gp", "<Plug>(YankyGPutAfter)", mode = { "n", "x" }, desc = "Put After Selection" },
+      { "gP", "<Plug>(YankyGPutBefore)", mode = { "n", "x" }, desc = "Put Before Selection" },
+
+      -- Cycle through yank history
+      { "[y", "<Plug>(YankyCycleForward)", desc = "Cycle Forward Yank" },
+      { "]y", "<Plug>(YankyCycleBackward)", desc = "Cycle Backward Yank" },
+
+      -- Indentation-aware put
+      { "]p", "<Plug>(YankyPutIndentAfterLinewise)", desc = "Put Indented After (Linewise)" },
+      { "[p", "<Plug>(YankyPutIndentBeforeLinewise)", desc = "Put Indented Before (Linewise)" },
+      { "]P", "<Plug>(YankyPutIndentAfterLinewise)", desc = "Put Indented After (Linewise)" },
+      { "[P", "<Plug>(YankyPutIndentBeforeLinewise)", desc = "Put Indented Before (Linewise)" },
+
+      { ">p", "<Plug>(YankyPutIndentAfterShiftRight)", desc = "Put + Shift Right" },
+      { "<p", "<Plug>(YankyPutIndentAfterShiftLeft)", desc = "Put + Shift Left" },
+      { ">P", "<Plug>(YankyPutIndentBeforeShiftRight)", desc = "Put Before + Shift Right" },
+      { "<P", "<Plug>(YankyPutIndentBeforeShiftLeft)", desc = "Put Before + Shift Left" },
+
+      -- With filters
+      { "=p", "<Plug>(YankyPutAfterFilter)", desc = "Put After + Filter" },
+      { "=P", "<Plug>(YankyPutBeforeFilter)", desc = "Put Before + Filter" },
+    },
+  },
+
+  {
+    "MagicDuck/grug-far.nvim",
+    cmd = "GrugFar",
+    opts = { headerMaxWidth = 80 },
+    keys = {
+      {
+        "<leader>sr",
+        function()
+          local grug = require "grug-far"
+          local ext = vim.bo.buftype == "" and vim.fn.expand "%:e"
+          grug.open {
+            transient = true,
+            prefills = {
+              filesFilter = ext and ext ~= "" and "*." .. ext or nil,
+            },
+          }
+        end,
+        mode = { "n", "v" },
+        desc = "Search and Replace",
       },
     },
   },
