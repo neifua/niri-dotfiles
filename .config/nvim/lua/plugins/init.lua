385a0323138f98d71b7e425873355e8e2fcbe58c
Neifua <ogkhang.05@gmail.com>
2025-07-09 22:31:58 +0700

    .

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -641,4 +641,14 @@ return {
       }
     end,
   },
+
+  {
+    "brianhuster/live-preview.nvim",
+    dependencies = {
+      -- You can choose one of the following pickers
+      "nvim-telescope/telescope.nvim",
+      "ibhagwan/fzf-lua",
+      "echasnovski/mini.pick",
+    },
+  },
 }
